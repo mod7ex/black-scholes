8382f4b6cccacb3418625e7e59721eec4054a26e
Mourad <mouradface1998@gmail.com>
2026-05-26 19:26:42 +0100

    save

---
+++ run.sh
@@ -0,0 +1,3 @@
+# python index.py                  # all nearby expirations
+python index.py --expiry 2026-05-30   # single expiry
+# python index.py --top 30         # show only top 30 strikes by |GEX|
